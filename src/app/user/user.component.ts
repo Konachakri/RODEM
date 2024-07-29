e81d026f5eadb7e89f589cd577b2e90ad1fa2e8a
konachakri <knschakradhar@gmail.com>
2024-07-29 10:28:15 +0530

    form array added

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
@@ -7,31 +7,51 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-  public userForm:FormGroup=new FormGroup({
-    name:new FormControl(null,[Validators.required,Validators.minLength(4),Validators.maxLength(10)]),
-    age:new FormControl(null,[Validators.required,Validators.min(0),Validators.max(100)]),
-    phone:new FormControl(null,[Validators.required,Validators.min(1000000000),Validators.max(9999999999)]),
-    email:new FormControl(null,[Validators.required,Validators.email]),
-    address:new FormGroup({
-      city:new FormControl(null,[Validators.required]),
-      pincode:new FormControl(null,[Validators.required])
+  public userForm: FormGroup = new FormGroup({
+    name: new FormControl(null, [Validators.required, Validators.minLength(4), Validators.maxLength(10)]),
+    age: new FormControl(null, [Validators.required, Validators.min(0), Validators.max(100)]),
+    phone: new FormControl(null, [Validators.required, Validators.min(1000000000), Validators.max(9999999999)]),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    address: new FormGroup({
+      city: new FormControl(null, [Validators.required]),
+      pincode: new FormControl(null, [Validators.required])
     }),
-    type:new FormControl(),
+    type: new FormControl(),
     // busfee:new FormControl(),
     // hostelfee:new FormControl()
-  })
 
-  constructor() { 
+    cards:new FormArray([])
+  })
+  get cardsFormArray(){
+    return this.userForm.get('cards') as FormArray;
+  }
+
+  addCard(){
+    this.cardsFormArray.push(
+      new FormGroup({
+          number: new FormControl(null,[Validators.required]),
+          expiry:new FormControl(null,[Validators.required]),
+          cvv:new FormControl(null,[Validators.required])
+      })
+    )
+  }
+
+  deletecard(i:number){
+    this.cardsFormArray.removeAt(i);
+
+  }
+
+  constructor() {
     this.userForm.get('type')?.valueChanges.subscribe(
-      (data:any)=>{
-        if(data=='Dayscholor'){
+      (data: any) => {
+        if (data == 'Dayscholor') {
           //add bus fee
-          this.userForm.addControl('busfee',new FormControl(null,[Validators.required,Validators.min(10000)]));
+          this.userForm.addControl('busfee', new FormControl(null, [Validators.required, Validators.min(10000)]));
           this.userForm.removeControl('hostelfee');
         }
-        else if(data=='Residential'){
+        else if (data == 'Residential') {
           //add hostel fee
-          this.userForm.addControl('hostelfee', new FormControl(null,[Validators.required,Validators.min(15000)]));
+          this.userForm.addControl('hostelfee', new FormControl(null, [Validators.required, Validators.min(15000)]));
           this.userForm.removeControl('busfee');
         }
       }
@@ -41,7 +61,7 @@ export class UserComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  submit(){
+  submit() {
     this.userForm.markAllAsTouched();
     console.log(this.userForm);
   }
